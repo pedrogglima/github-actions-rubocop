d2abec8161e978db47cf575aeced350f3d5bc2c8
Pedro Lima <pedrogglima@gmail.com>
2023-06-30 14:37:50 -0300

    chore: add gem install rubocop-packaging

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -3,8 +3,10 @@
 set -e
 
 gem install rubocop -v 1.51.0
+gem install rubocop-packaging -v 0.5.2
 gem install rubocop-rspec -v 2.22.0
 gem install rubocop-performance -v 1.18.0
 gem install rubocop-rails -v 2.20.2
+
 
 ruby /action/lib/index.rb
